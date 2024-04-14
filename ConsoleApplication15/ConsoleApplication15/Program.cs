4de63dd0a18e1d9332ede42b3d8690342d5aade2
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2024-04-14 13:51:05 +0530

    Add files via upload

---
+++ ConsoleApplication15/ConsoleApplication15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1= new A();
+            mydelegate m = new mydelegate(a1.addnum);
+            mydelegate n = new mydelegate(a1.subnum);
+            mydelegate s= new mydelegate(a1.mulnum);
+            m(4);
+            n(6);
+            s(4);
+        }
+    }
+      public delegate void mydelegate(int a);
+     class A
+    {
+        int a=54, b=89, add, sub, mul;
+        float div;
+       public void addnum(int d)
+        {
+            add = a + b;
+            Console.WriteLine("add:"+add);
+
+        }
+      public void subnum( int y)
+       {
+         sub = a - b;
+       Console.WriteLine("sub:"+sub);
+      }
+       public void mulnum(int x)
+       {
+          mul=a*b;
+          Console.WriteLine("mul:" + mul);
+
+       }
+
+
+    }
+}
+
