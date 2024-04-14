225b953837ad1dc29dd95367e66c5488088716dc
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2024-04-14 13:50:03 +0530

    Add files via upload

---
+++ ConsoleApplication14/ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            A a1 = new A();
+            a1.accept();
+            a1.calculate();
+            a1.display();
+        }
+    }
+    interface area
+    {
+        
+
+        void accept();
+        void calculate();
+        void display();
+    
+    }
+    class A: area 
+    {
+        
+        public int l, b;
+        public float area;
+    
+       public void accept()
+        {
+            Console.WriteLine("enter length:");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter breadth:");
+            b = Convert.ToInt32(Console.ReadLine());
+                
+        }
+       public void calculate()
+        {
+         area = l*b;
+    Console.WriteLine("area is:"+area);
+        
+        
+        }
+
+      public void display()
+    {
+
+        Console.Write("length is:"+l);
+        Console.Write("breadth :"+b);
+        Console.Write("area :" + area);
+    
+    }
+        }
+
+    
+    
+    
+}
---
+++ ConsoleApplication14/ConsoleApplication14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+
+
+    class program
+    {
+      public static void main(string[] args)
+      {
+          A a1 = new A();
+          mydelegate m = new mydelegate(a1.addnum);
+          mydelegate n = new mydelegate(a1.subnum);
+          mydelegate s = new mydelegate(a1.mulnum);
+          m(10);
+          n(30);
+          s(46);
+      }
+    
+    }
+    public delegate void mydelegate(int a);
+     class A
+    {
+        int a, b, add, sub, mul;
+        float div;
+        void addnum(int a)
+        {
+            add = a + b;
+            Console.WriteLine("add:"+add);
+
+        }
+       void subnum(int c)
+       {
+         sub = a - b;
+       Console.WriteLine("sub"+sub);
+      }
+        void mul(int x)
+       {
+          mul=a*b;
+    Console.WriteLine("mul:"+mul);
+
+       }
+
+
+    }
+}
